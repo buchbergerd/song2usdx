429d85e1250aeb58a795d6f2b388343203d45586
Dennis Buchberger <75340117+buchbergerd@users.noreply.github.com>
2024-09-25 18:10:26 +0200

    Added demo songs

---
+++ extract_vocals_1.sh
@@ -0,0 +1,2 @@
+audio-separator "Examples/Evanecence - Bring me to life/Evanecense - Bring me to life.mp3" --model_filename UVR-MDX-NET-Inst_HQ_3.onnx
+
---
+++ extract_vocals_2.sh
@@ -0,0 +1,2 @@
+python vocal-remover/inference.py --input "../Examples/Evanecence - Bring me to life/Evanecense - Bring me to life.mp3" --tta
+
